e7e49885c9fc69ecae2dddcb62d582047a651b49
KSPOG <kratosmaximusosrs@gmail.com>
2026-07-17 22:52:04 +0200

    Route Pages API through Discord-aware worker

--- marketplace-site/functions/api/[[path]].js
+++ marketplace-site/functions/api/[[path]].js
@@ -1,4 +1,4 @@
-import marketplaceWorker from "../../worker/index.js";
+import marketplaceWorker from "../../worker/discord-worker.js";
 import { handlePagesAuth } from "../../worker/pages-auth.js";
 import { handlePasswordReset } from "../../worker/pages-password-reset.js";
 import { handleControlPlane } from "../../worker/pages-control-plane.js";
