cf588280dfe7278a5f47bc1d4bc2814ae924f5e6
KSPOG <kratosmaximusosrs@gmail.com>
2026-07-17 04:24:55 +0200

    Show Shortest Path walker status in overlay

--- runelite-client/src/main/java/net/runelite/client/plugins/klite/KLiteOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/klite/KLiteOverlay.java
@@ -9,8 +9,11 @@ import java.awt.Dimension;
 import java.awt.Graphics2D;
 import javax.inject.Inject;
 import net.runelite.api.Client;
+import net.runelite.api.coords.WorldPoint;
 import net.runelite.client.RuneLiteProperties;
 import net.runelite.client.plugins.klite.automation.AutomationManager;
+import net.runelite.client.plugins.klite.walker.WebWalkResult;
+import net.runelite.client.plugins.klite.walker.WebWalker;
 import net.runelite.client.ui.overlay.OverlayPanel;
 import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.components.LineComponent;
@@ -21,13 +24,16 @@ final class KLiteOverlay extends OverlayPanel
 	private final Client client;
 	private final KLiteConfig config;
 	private final AutomationManager automationManager;
+	private final WebWalker webWalker;
 
 	@Inject
-	private KLiteOverlay(Client client, KLiteConfig config, AutomationManager automationManager)
+	private KLiteOverlay(Client client, KLiteConfig config, AutomationManager automationManager,
+		WebWalker webWalker)
 	{
 		this.client = client;
 		this.config = config;
 		this.automationManager = automationManager;
+		this.webWalker = webWalker;
 		setPosition(OverlayPosition.TOP_LEFT);
 	}
 
@@ -54,6 +60,27 @@ final class KLiteOverlay extends OverlayPanel
 			.left("Automation")
 			.right(automationManager.status().getState().toString())
 			.build());
+
+		WebWalkResult walk = webWalker.status();
+		panelComponent.getChildren().add(LineComponent.builder()
+			.left("Web walker")
+			.right(walk.getState().toString())
+			.build());
+		if (walk.getPathLength() > 0)
+		{
+			panelComponent.getChildren().add(LineComponent.builder()
+				.left("Shortest Path")
+				.right(walk.getPathLength() + " tiles")
+				.build());
+		}
+		WorldPoint next = walk.getClickTarget();
+		if (next != null)
+		{
+			panelComponent.getChildren().add(LineComponent.builder()
+				.left("Next route tile")
+				.right(next.getX() + ", " + next.getY() + ", " + next.getPlane())
+				.build());
+		}
 
 		return super.render(graphics);
 	}
