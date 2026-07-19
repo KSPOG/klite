95db77cd8d6a05f93ddc44fa46f852d19f049987
KSPOG <kratosmaximusosrs@gmail.com>
2026-07-19 18:01:45 +0200

    Test world map escape close events

--- runelite-client/src/test/java/net/runelite/client/plugins/klite/walker/KLiteWorldMapWebWalkerControllerTest.java
+++ runelite-client/src/test/java/net/runelite/client/plugins/klite/walker/KLiteWorldMapWebWalkerControllerTest.java
@@ -7,8 +7,11 @@ package net.runelite.client.plugins.klite.walker;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
 
+import java.awt.Canvas;
 import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
 import net.runelite.api.Point;
 import net.runelite.api.coords.WorldPoint;
 import org.junit.Test;
@@ -45,6 +48,27 @@ public class KLiteWorldMapWebWalkerControllerTest
 			new Rectangle(), MAP_POSITION, 4.0f, new Point(302, 200)));
 		assertNull(KLiteWorldMapWebWalkerController.worldPointForMapClick(
 			MAP_BOUNDS, MAP_POSITION, 4.0f, new Point(99, 200)));
+	}
+
+	@Test
+	public void closeMapUsesEscapePressAndReleaseEvents()
+	{
+		Canvas canvas = new Canvas();
+		long when = 1234L;
+		KeyEvent pressed = KLiteWorldMapWebWalkerController.createEscapeEvent(
+			canvas, KeyEvent.KEY_PRESSED, when);
+		KeyEvent released = KLiteWorldMapWebWalkerController.createEscapeEvent(
+			canvas, KeyEvent.KEY_RELEASED, when);
+
+		assertSame(canvas, pressed.getSource());
+		assertEquals(KeyEvent.KEY_PRESSED, pressed.getID());
+		assertEquals(KeyEvent.VK_ESCAPE, pressed.getKeyCode());
+		assertEquals(KeyEvent.CHAR_UNDEFINED, pressed.getKeyChar());
+		assertEquals(when, pressed.getWhen());
+		assertSame(canvas, released.getSource());
+		assertEquals(KeyEvent.KEY_RELEASED, released.getID());
+		assertEquals(KeyEvent.VK_ESCAPE, released.getKeyCode());
+		assertEquals(when, released.getWhen());
 	}
 
 	@Test
